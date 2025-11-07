ad6fa38a873cf1cc6c5a36d2c4ec49c38de8f467
Hassan Tarek <hassanelashry415@gmail.com>
2025-11-07 16:24:57 +0200

    Created repositories for User, Role, and RefreshToken entities

---
+++ src/main/java/com/jwt/auth/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.jwt.auth.repository;
+
+import com.jwt.auth.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends
+        JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+}
